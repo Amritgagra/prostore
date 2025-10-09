bf6bfdc370e3d6d85d95abd59ac58dee490a4000
Amritpal Singh <amritgagra200@gmail.com>
2025-10-09 23:24:17 +0530

    add header and footer

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t">
+        <div className="wrapper py-4 text-center text-sm text-muted-foreground">
+            &copy; 2024 E-Commerce. All rights reserved.
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
